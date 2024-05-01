a9ce7a59b39a8ca527cb0da7ae905092765ea956
Alexa Chavez <aguinada14@gmail.com>
2024-05-01 14:23:33 -0700

    Added starter code in Contact page with input fields for name, email and message

---
+++ src/sections/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <form>
+        <input type="text" placeholder="Name" />
+        <input type="email" placeholder="Email" />
+        <textarea placeholder="Message"></textarea>
+        <button>Send Message</button>
+      </form>
+    </section>
+  );
+};
+
+export default Contact;
