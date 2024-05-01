5b1ee7cf9c7fba2b62bbcffecb31435d767c41ad
Alexa Chavez <aguinada14@gmail.com>
2024-05-01 14:05:20 -0700

    Added starter code in footer component with external profile links

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <a href="https://github.com/achavez14" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+        {/* Add another platform link */}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
