d2907cd4d9ae087c48973147bdadcc7e579762dd
Alexa Chavez <aguinada14@gmail.com>
2024-05-01 18:53:20 -0700

    Updated main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,4 @@
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import AboutMe from './pages/AboutMe';
-import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
-import Resume from './pages/Resume';
 
-// Define your routes
-const routes = [
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      { path: 'aboutme', element: <AboutMe /> },
-      { path: 'portfolio', element: <Portfolio /> },
-      { path: 'contact', element: <Contact /> },
-      { path: 'resume', element: <Resume /> },
-    ],
-  },
-];
-
-// Render your app with the Router and Routes
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
-    <Routes>
-      {routes.map((route, index) => (
-        <Route key={index} path={route.path} element={route.element}>
-          {route.children && route.children.map((childRoute, childIndex) => (
-            <Route key={childIndex} path={childRoute.path} element={childRoute.element} />
-          ))}
-        </Route>
-      ))}
-    </Routes>
-  </Router>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
