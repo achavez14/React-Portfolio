fcf5592f9aff728cb002db11082c44702413c8a4
Alexa Chavez <aguinada14@gmail.com>
2024-05-02 12:50:42 -0700

    Started implementing content with details about myself in AboutMe.jsx

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -3,12 +3,7 @@ export default function AboutMe() {
     <div>
       <h1>AboutMe Page</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      Hello, My name is Alexa Chavez! I am from California, and I am in the process of becoming a full-stack software developer.
       </p>
     </div>
   );
