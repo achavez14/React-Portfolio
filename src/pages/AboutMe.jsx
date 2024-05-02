99dd35db957c429f50c60c0114e36c1016c27bfc
Alexa Chavez <aguinada14@gmail.com>
2024-05-01 18:14:06 -0700

    Updated AboutMe.jsx file

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-
-const AboutMe = () => {
+export default function About() {
   return (
-    <section>
-      <h2>About Me</h2>
-      <img src="your-avatar.jpg" alt="Your Avatar" />
-      <p>Your short bio goes here. Talk about your skills, experiences, and passions.</p>
-    </section>
+    <div>
+      <h1>AboutMe Page</h1>
+      <p>
+        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
+        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
+        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
+        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
+        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
+        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      </p>
+    </div>
   );
-};
-
-export default AboutMe;
+}
