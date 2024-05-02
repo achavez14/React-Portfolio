d952cd98192e9e3cb5da8620c28131327c33db3d
Alexa Chavez <aguinada14@gmail.com>
2024-05-01 17:57:49 -0700

    Reorganized project structure after implementing Create Vite tool

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
-import React from 'react';
-import Header from './components/Header';
-import AboutMe from './sections/AboutMe';
-import Portfolio from './sections/Portfolio';
-import Contact from './sections/Contact';
-import Resume from './sections/Resume';
-import './styles/styles.css'; // Import CSS styles
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="app">
-      <Header />
-      <AboutMe />
-      <Portfolio />
-      <Contact />
-      <Resume />
-    </div>
-  );
-};
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-export default App;
+export default App
