387ecde60f6f90ee45e4f25ba19dac0da0647ade
Alexa Chavez <aguinada14@gmail.com>
2024-05-07 10:59:33 -0700

    Updated introduction in AboutMe.jsx

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,10 +1,17 @@
-export default function AboutMe() {
+import React from "react";
+
+const AboutMe = () => {
   return (
     <div>
-      <h1>AboutMe Page</h1>
+      <h1>About Me</h1>
       <p>
-      Hello, My name is Alexa Chavez! I am from California, and I am in the process of becoming a full-stack software developer.
+      Hello There! My name is Alexa Chavez, and I am a dedicated full-stack software developer based in California. I succumbed to my coding curiosity and embarked on a beautiful journey that introduced me to a new digital desire.
+      </p>
+      <p>
+        I thrive on solving real-world problems and learning new technologies, and as a full-stack software developer, the opportunities are endless!
       </p>
     </div>
   );
-}
+};
+
+export default AboutMe;
