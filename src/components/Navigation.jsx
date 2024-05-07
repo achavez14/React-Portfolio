0e10a3827f57d7c0e69b853ecb006766b81128cc
Alexa Chavez <aguinada14@gmail.com>
2024-05-07 10:39:06 -0700

    Updated Navigation.jsx for proper list semantics

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,10 +2,10 @@ const Navigation = () => {
   return (
     <nav>
       <ul>
-        <li>About Me</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
-        <li>Resume</li>
+        <li><a href="#about">About Me</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#resume">Resume</a></li>
       </ul>
     </nav>
   );
